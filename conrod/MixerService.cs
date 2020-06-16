0c229fa948ed1fcb0a01b1454b3c8ec450d058c8
Sam-Hogan <>
2020-06-16 23:53:44 +1200

    can play song files (index based)

--- conrod/MixerService.cs
+++ conrod/MixerService.cs
@@ -1,11 +1,32 @@
-﻿using System.Threading;
+﻿using CSCore;
+using CSCore.Codecs;
+using CSCore.SoundOut;
+using CSCore.Streams;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace conrod
 {
     public class MixerService: SetIfDifferentHelper
     {
         private readonly CommandStack _commandStack;
+        private bool _deckALoading = false;
+        public bool DeckALoading { get => _deckALoading; private set => SetIfDifferent(ref _deckALoading, value); }
+        private string _deckAFilename = null;
+        public string DeckAFilename { get => _deckAFilename; set
+            {
+                DeckALoading = true;
+                SetIfDifferent(ref _deckAFilename, value);
+                IWaveSource waveSource = CodecFactory.Instance.GetCodec(_deckAFilename);
+                _deckASoundOut?.Dispose();
+                _deckASoundOut = new DirectSoundOut();
+                _deckASoundOut.Initialize(waveSource);
+                _deckASoundOut.Play();
+            } }
 
+        private DirectSoundOut _deckASoundOut = null;
         public MixerService(CommandStack commandStack)
         {
             _commandStack = commandStack;
@@ -29,7 +50,12 @@ namespace conrod
 
         private void ProcessCommand(Command command)
         {
-
+            if (command.Location == "load")
+            {
+                string filename = Directory.GetFiles($"C:\\Users\\Sam\\Documents\\Coding\\Slider-Crank\\cylinder\\library\\")[command.Value];
+                Console.WriteLine($"Trying to load from {filename}");
+                DeckAFilename = filename;
+            }
         }
     }
 }
---
+++ crank_pin/demo4.py
@@ -0,0 +1,41 @@
+import socket
+import time
+import json
+
+HOST = '127.0.0.1'
+PORT = 726
+
+DATA_ON = f'{json.dumps({"power": 1})}\n'.encode('utf-8')
+DATA_OFF = f'{json.dumps({"power": 0})}\n'.encode('utf-8')
+DATA_EOF = b'<EOF>'
+
+
+DATA_LOAD = lambda x: f'{json.dumps({"load": x})}\n'.encode('utf-8')
+
+
+def initialise():
+    while True:
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((HOST, PORT))
+            return sock
+        except:
+            print('Error connecting to socket, trying again....')
+            time.sleep(3)
+
+
+def send_bytes(data: bytes, sock: socket.SocketType):
+    sock.send(data)
+    print(f'Successfully sent {data}')
+
+
+def main():
+        sock = initialise()
+        while True:
+            send_bytes(DATA_LOAD(int(input("Enter song number: "))), sock)
+            # send_bytes(DATA_EOF, sock)
+        sock.close()
+
+
+if __name__ == "__main__":
+    main()
